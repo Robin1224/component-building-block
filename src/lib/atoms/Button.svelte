4010a7fe5d824e74d74f7d634a2a8f833411247e
Robin <robin.django@gmail.com>
2024-11-25 15:52:55 +0100

    Added reusable button component

---
+++ src/lib/atoms/Button.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let bgColor = "--vtYellow";
+  export let color = "#000";
+
+  // If href is given, the button will be a link
+  export let href = "";
+
+  export let className = "";
+</script>
+
+<!-- Check if it's a link or not. If it is, render an <a> instead of an <button> -->
+{#if href}
+  <a {href} style="--bg: var({bgColor}); --color: {color}" class={className}>
+    <slot></slot>
+  </a>
+{:else}
+  <button style="--bg: var({bgColor}); --color: {color}" class={className}>
+    <slot></slot>
+  </button>
+{/if}
+
+<style>
+  a, button {
+    background-color: var(--bg);
+    color: var(--color);
+    padding: 0.4rem;
+    cursor: pointer;
+    border-radius: 3px;
+    border: none;
+    height: fit-content;
+    width: fit-content;
+  }
+</style>
