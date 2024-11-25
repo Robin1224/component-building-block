92d47a6c327e05b91fe7e5cf68464402c3a5a11b
Robin <robin.django@gmail.com>
2024-11-25 15:53:07 +0100

    Created first iteration of cards list

---
+++ src/lib/molecules/Card.svelte
@@ -0,0 +1,79 @@
+<script>
+  import Button from "$lib/atoms/Button.svelte";
+  import Arrows from "$lib/icons/Arrows.svelte";
+
+  export let method;
+</script>
+
+<li>
+  <a href="/tekenmethodes/{method.slug}">
+    {#if method.template && method.template.url}
+      <img
+        src={method.template.url.replace(":webp", ":png")}
+        alt={"Voorbeeld van " + method.title}
+        class={method.categories[0].title.replaceAll(" ", "-")}
+      />
+    {:else}
+      <img
+        class={method.categories[0].title.replaceAll(" ", "-")}
+        src="/placeholder.webp"
+        alt="Placeholder"
+      />
+    {/if}
+
+    <h3>{method.title}</h3>
+    <Button className="card-button">
+      <Arrows styles="transform: translateY(2px) translateX(1px);" />
+    </Button>
+  </a>
+</li>
+
+<style>
+  li {
+    list-style: none;
+    width: 100%;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+    margin-bottom: 5px;
+    padding: 0.8em;
+    border-bottom: 1px solid #ccc;
+    display: grid;
+    grid-template-columns: 10rem 1fr;
+    grid-template-rows: 1fr 1fr;
+    grid-template-areas:
+      "img title"
+      "img button";
+  }
+
+  img {
+    width: 10rem;
+    height: auto;
+
+    grid-area: img;
+  }
+
+  h3 {
+    font-family: var(--vtPrimaryFont);
+    color: var(--vtDarkBlue);
+    font-size: 24px;
+    font-weight: 400;
+    margin: 0;
+    width: 100%;
+    padding-left: 1rem;
+
+    grid-area: title;
+  }
+
+  :global(.card-button) {
+    margin-left: auto;
+    margin-top: auto;
+
+    grid-area: button;
+  }
+
+  @container (min-width: 36rem) {
+  }
+</style>
---
+++ src/lib/molecules/CardsList.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Card from "./Card.svelte";
+
+  export let data;
+</script>
+
+<section>
+  <h2>Alle methodes ({data.length})</h2>
+  <ul>
+    {#if data && data.length > 0}
+      {#each data as method}
+        <Card {method} />
+      {/each}
+    {/if}
+  </ul>
+</section>
+
+<style>
+  section, ul {
+    width: 100%;
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: auto;
+    margin: 1rem 0;
+    container-type: inline-size;
+  }
+
+  @media screen and (min-width: 36em) {
+    ul {
+      grid-template-columns: repeat(2, 1fr);
+      gap: 1.5rem;
+    }
+  }
+
+  @media screen and (min-width: 60em) {
+    ul {
+      grid-template-columns: repeat(3, auto);
+    }
+  }
+</style>
